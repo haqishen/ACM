316d12c6ad78c6a01e77b77b3808bed9885d371c
haqishen <haqishen@gmail.com>
2015-06-06 14:35:54 +0900

    ACM question in POJ, No.1113
    http://poj.org/problem?id=1113
    
    Description
    
    Once upon a time there was a greedy King who ordered his chief Architect
    to build a wall around the King's castle. The King was so greedy, that
    he would not listen to his Architect's proposals to build a beautiful
    brick wall with a perfect shape and nice tall towers. Instead, he
    ordered to build the wall around the whole castle using the least amount
    of stone and labor, but demanded that the wall should not come closer to
    the castle than a certain distance. If the King finds that the Architect
    has used more resources to build the wall than it was absolutely
    necessary to satisfy those requirements, then the Architect will loose
    his head. Moreover, he demanded Architect to introduce at once a plan of
    the wall listing the exact amount of resources that are needed to build
    the wall.
    
    Your task is to help poor Architect to save his head, by writing a
    program that will find the minimum possible length of the wall that he
    could build around the castle to satisfy King's requirements.
    
    The task is somewhat simplified by the fact, that the King's castle has
    a polygonal shape and is situated on a flat ground. The Architect has
    already established a Cartesian coordinate system and has precisely
    measured the coordinates of all castle's vertices in feet.
    Input
    
    The first line of the input file contains two integer numbers N and L
    separated by a space. N (3 <= N <= 1000) is the number of vertices in
    the King's castle, and L (1 <= L <= 1000) is the minimal number of feet
    that King allows for the wall to come close to the castle.
    
    Next N lines describe coordinates of castle's vertices in a clockwise
    order. Each line contains two integer numbers Xi and Yi separated by a
    space (-10000 <= Xi, Yi <= 10000) that represent the coordinates of ith
    vertex. All vertices are different and the sides of the castle do not
    intersect anywhere except for vertices.
    Output
    
    Write to the output file the single number that represents the minimal
    possible length of the wall in feet that could be built around the
    castle to satisfy King's requirements. You must present the integer
    number of feet to the King, because the floating numbers are not
    invented yet. However, you must round the result in such a way, that it
    is accurate to 8 inches (1 foot is equal to 12 inches), since the King
    will not tolerate larger error in the estimates.
    Sample Input
    
    9 100
    200 400
    300 400
    300 300
    400 300
    400 400
    500 400
    500 200
    350 200
    200 200
    Sample Output
    
    1628

---
+++ Wall1113.java
@@ -0,0 +1,109 @@
+package Geometry;
+import java.io.*;
+import java.util.*;
+public class Wall1113 {
+	static int N,L;
+	static int[] x,y;
+	static vertice vs[];
+	static ArrayList<Integer> al = new ArrayList<Integer>();
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String input[] = br.readLine().split(" +");
+		N = new Integer(input[0]);
+		L = new Integer(input[1]);
+		
+		x = new int[N];
+		y = new int[N];
+		vs = new vertice[N];
+		
+		for(int i = 0; i < N; i++){
+			input = br.readLine().split(" +");
+			x[i] = new Integer(input[0]);
+			y[i] = new Integer(input[1]);
+			vs[i] = new vertice(i, x[i], y[i]);
+		}
+		Arrays.sort(vs);
+		
+		for(int i = 0; i < N; i++){
+			int no = vs[i].no;
+			while(al.size()>1){
+				if(!check(no))
+					al.remove(al.size()-1);
+				else
+					break;
+			}
+			if(!al.contains(no))
+				al.add(no);
+		}
+//		for(int i : al){
+//			System.out.println(i);
+//		}System.out.println();
+		int sz = al.size();
+		for(int i = N-2; i >= 0; i--){
+			int no = vs[i].no;
+			while(al.size()>sz){
+				if(!check(no))
+					al.remove(al.size()-1);
+				else
+					break;
+			}
+			if(!al.contains(no))
+				al.add(no);
+		}
+//		for(int i : al){
+//			System.out.println(i);
+//		}
+		
+		double perimeter = 0;
+		for(int i = 0; i < al.size()-1; i++){
+			perimeter += dist(i,i+1);
+//			System.out.println(perimeter);
+		}
+		if(al.size()>1){
+			perimeter += dist(0, al.size()-1);
+		}
+		perimeter += 2*L*Math.PI;		
+		System.out.println(Math.round(perimeter));
+	}
+
+	static double dist(int i, int j){
+		int no1 = al.get(i);
+		int no2 = al.get(j);
+		int x1 = x[no1] - x[no2];
+		int y1 = y[no1] - y[no2];
+		return Math.sqrt(x1*x1 + y1*y1);
+	}
+
+	static boolean check(int i){
+//		System.out.println(i);
+		int len = al.size();
+		int nol = al.get(len-1);
+		int noll = al.get(len-2);
+		int x1 = x[nol] - x[noll];
+		int y1 = y[nol] - y[noll];
+		int x2 = x[i] - x[nol];
+		int y2 = y[i] - y[nol];
+		if(x1*y2 - x2*y1 >=0){
+			return false;
+		}else{
+			return true;
+		}
+	}
+}
+
+class vertice implements Comparable<vertice>{
+	int no, x, y;
+	vertice(int no, int x, int y){
+		this.no = no;
+		this.x = x;
+		this.y = y;
+	}
+	public int compareTo(vertice o) {
+		if(this.x == o.x)
+			return this.y - o.y;
+		else
+			return this.x - o.x;
+	}
+}
