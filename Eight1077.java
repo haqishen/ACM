6bc8ad52e9804cd5644da69c9fe1db57e5f04a33
haqishen <haqishen@gmail.com>
2015-06-06 14:37:37 +0900

    ACM question in POJ, No.1077
    http://poj.org/problem?id=1077
    
    Eight
    Time Limit: 1000MS  Memory Limit: 65536K
    Total Submissions: 26661  Accepted: 11657  Special Judge
    Description
    
    The 15-puzzle has been around for over 100 years; even if you don't know
    it by that name, you've seen it. It is constructed with 15 sliding
    tiles, each with a number from 1 to 15 on it, and all packed into a 4 by
    4 frame with one tile missing. Let's call the missing tile 'x'; the
    object of the puzzle is to arrange the tiles so that they are ordered
    as:
    1  2  3  4
    
    5  6  7  8
    
    9 10 11 12
    
    13 14 15  x
    
    where the only legal operation is to exchange 'x' with one of the tiles
    with which it shares an edge. As an example, the following sequence of
    moves solves a slightly scrambled puzzle:
    1  2  3  4    1  2  3  4    1  2  3  4    1  2  3  4
    
    5  6  7  8    5  6  7  8    5  6  7  8    5  6  7  8
    
    9  x 10 12    9 10  x 12    9 10 11 12    9 10 11 12
    
    13 14 11 15   13 14 11 15   13 14  x 15   13 14 15  x
    
    r->           d->           r->
    
    The letters in the previous row indicate which neighbor of the 'x' tile
    is swapped with the 'x' tile at each step; legal values are 'r','l','u'
    and 'd', for right, left, up, and down, respectively.
    
    Not all puzzles can be solved; in 1870, a man named Sam Loyd was famous
    for distributing an unsolvable version of the puzzle, and
    frustrating many people. In fact, all you have to do to make a regular
    puzzle into an unsolvable one is to swap two tiles (not counting the
    missing 'x' tile, of course).
    
    In this problem, you will write a program for solving the less
    well-known 8-puzzle, composed of tiles on a three by three
    arrangement.
    Input
    
    You will receive a description of a configuration of the 8 puzzle. The
    description is just a list of the tiles in their initial positions, with
    the rows listed from top to bottom, and the tiles listed from left to
    right within a row, where the tiles are represented by numbers 1 to 8,
    plus 'x'. For example, this puzzle
    1  2  3
    
    x  4  6
    
    7  5  8
    
    is described by this list:
    
    1 2 3 x 4 6 7 5 8
    Output
    
    You will print to standard output either the word ``unsolvable'', if the
    puzzle has no solution, or a string consisting entirely of the letters
    'r', 'l', 'u' and 'd' that describes a series of moves that produce a
    solution. The string should include no spaces and start at the beginning
    of the line.
    Sample Input
    
    2  3  4  1  5  x  7  6  8
    Sample Output
    
    ullddrurdllurdruldr

---
+++ Eight1077.java
@@ -0,0 +1,89 @@
+package BFS;
+
+import java.io.*;
+import java.util.*;
+
+public class Eight1077 {
+	
+	static long pow[] = {100000000,10000000,1000000,100000,10000,1000,100,10,1};
+	static long goal = 123456780;
+	static String res = "";
+	static ArrayDeque<Long> pat_que = new ArrayDeque<Long>();
+	static ArrayDeque<String> ans_que = new ArrayDeque<String>();
+	static HashMap<Long,Integer> history =  new HashMap<Long,Integer>();
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String input[] = br.readLine().trim().split(" +");
+		long ini = 0;
+		for(int x = 0; x < 9; x++){
+			if(!input[x].equals("x")){
+				long l = new Long(input[x]);
+				ini += l * pow[x];
+			}
+		}
+		
+		String A = "unsolvable";
+		pat_que.add(ini);
+		history.put(ini,0);
+		ans_que.add("");
+		while(pat_que.size()>0){
+			long pat = pat_que.pollFirst();
+			String ans = ans_que.pollFirst();
+//			debug(pat);
+			if(pat == goal){
+				A = ans;
+				break;
+			}
+			
+//			find out the position of 0
+			int s = 0;
+			for(int i = 0; i < 9; i++)
+				if((pat/pow[i])%10 == 0)
+					s = i;
+			
+			for(int i = 0; i < 4; i++){
+				
+				long newpat = pat;
+				if(i==0){
+					if(s==2|s==5|s==8) continue;
+					long a = (newpat/pow[s+1])%10;
+					newpat += a * (pow[s] - pow[s+1]);
+				}else if(i==1){
+					if(s==0|s==3|s==6) continue;
+					long a = (newpat/pow[s-1])%10;
+					newpat += a * (pow[s] - pow[s-1]);
+				}else if(i==2){
+					if(s==6|s==7|s==8) continue;
+					long a = (newpat/pow[s+3])%10;
+					newpat += a * (pow[s] - pow[s+3]);
+				}else if(i==3){
+					if(s==0|s==1|s==2) continue;
+					long a = (newpat/pow[s-3])%10;
+					newpat += a * (pow[s] - pow[s-3]);
+				}
+
+				if(!history.containsKey(newpat)){
+					history.put(newpat,0);
+//					debug(copy);
+					pat_que.addLast(newpat);
+					if(i==0)
+						ans_que.addLast(ans+"r");
+					if(i==1)
+						ans_que.addLast(ans+"l");
+					if(i==2)
+						ans_que.addLast(ans+"d");
+					if(i==3)
+						ans_que.addLast(ans+"u");
+				}
+			}
+//			debug(pat_que.size());
+		}
+		System.out.println(A);
+	}
+
+	static void debug(Object...o){
+		System.err.println(Arrays.deepToString(o));
+	}
+}
