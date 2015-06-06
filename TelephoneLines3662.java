7019f53b99798d289fe6540a4a54e03607f22bd1
haqishen <haqishen@gmail.com>
2015-06-06 14:24:57 +0900

    ACM question in POJ, No.3662
    http://poj.org/problem?id=3662
    
    Description
    
    Farmer John wants to set up a telephone line at his farm. Unfortunately,
    the phone company is uncooperative, so he needs to pay for some of the
    cables required to connect his farm to the phone system.
    
    There are N (1 ≤ N ≤ 1,000) forlorn telephone poles conveniently
    numbered 1..N that are scattered around Farmer John's property; no
    cables connect any them. A total of P (1 ≤ P ≤ 10,000) pairs of poles
    can be connected by a cable; the rest are too far apart.
    
    The i-th cable can connect the two distinct poles Ai and Bi, with length
    Li (1 ≤ Li ≤ 1,000,000) units if used. The input data set never names
    any {Ai, Bi} pair more than once. Pole 1 is already connected to the
    phone system, and pole N is at the farm. Poles 1 and N need to be
    connected by a path of cables; the rest of the poles might be used or
    might not be used.
    
    As it turns out, the phone company is willing to provide Farmer John
    with K (0 ≤ K < N) lengths of cable for free. Beyond that he will have
    to pay a price equal to the length of the longest remaining cable he
    requires (each pair of poles is connected with a separate cable), or 0
    if he does not need any additional cables.
    
    Determine the minimum amount that Farmer John must pay.
    
    Input
    
    * Line 1: Three space-separated integers: N, P, and K
    * Lines 2..P+1: Line i+1 contains the three space-separated integers:
    Ai, Bi, and Li
    
    Output
    
    * Line 1: A single integer, the minimum amount Farmer John can pay. If
    it is impossible to connect the farm to the phone company, print -1.
    
    Sample Input
    
    5 7 1
    1 2 5
    3 1 4
    2 4 8
    3 2 3
    5 2 9
    3 4 7
    4 5 6
    Sample Output
    
    4

---
+++ TelephoneLines3662.java
@@ -0,0 +1,119 @@
+package Dijkstra;
+
+import java.io.*;
+import java.util.*;
+
+public class TelephoneLines3662 {
+	static final int INF = 1000000001;
+	static int N,P,K;
+	static boolean done[];
+	static tele ts[];
+	static int roads[][];
+	static LinkedList<line> ll[];
+	static PriorityQueue<tele> pq;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		 
+		pq = new PriorityQueue<tele>();
+		
+		String input[] = br.readLine().split("\\s");
+		N = new Integer(input[0]);
+		P = new Integer(input[1]);
+		K = new Integer(input[2]);
+		
+		done = new boolean[N+1];
+		ts = new tele[N+1];
+		roads = new int[3][P];
+		ll = new LinkedList[N+1];
+		
+		for(int i = 0; i <= N; i++){
+			ts[i] = new tele(i);
+			ll[i] = new LinkedList<line>();
+		}
+		
+		for(int i = 0; i < P; i++){
+			input = br.readLine().split("\\s");
+			roads[0][i] = new Integer(input[0]);
+			roads[1][i] = new Integer(input[1]);
+			roads[2][i] = new Integer(input[2]);
+		}
+		
+		int l = -1;
+		int r = INF;
+		while(r - l > 1){
+			int m = (l+r)/2;
+			if(check(m)){
+				r = m;
+			}else{
+				l = m;
+			}
+		}
+		System.out.println(r);
+	}
+	private static boolean check(int m) {
+//		System.out.println("m: "+m);
+
+		for(int i = 0; i <= N; i++){
+			ts[i] = new tele(i);
+			done[i] = false;
+			ll[i].clear();
+		}
+
+
+		for(int i = 0; i < P; i++){
+			int t = roads[2][i] > m ? 1 : 0;
+			ll[roads[0][i]].add(new line(roads[1][i], t));
+			ll[roads[1][i]].add(new line(roads[0][i], t));
+		}
+		
+//		dijkstra
+		pq.clear();
+		ts[1].dist = 0;
+		pq.add(ts[1]);
+		
+		while(!done[N]){
+			if(pq.size() < 1) break;
+			tele te = pq.poll();
+			if(done[te.no]) continue;
+			done[te.no] = true;			
+			for(line l : ll[te.no]){
+				int d = te.dist + l.dist;
+				if(d < ts[l.to].dist){
+					ts[l.to].dist = d;
+					pq.add(ts[l.to]);
+				}				
+			}
+		}
+		
+
+
+		if(ts[N].dist == INF){
+			System.out.println(-1);
+			System.exit(0);
+		}
+		
+
+		if(ts[N].dist <= K){
+			return true;
+		}else{
+			return false;
+		}		
+	}
+}
+
+class tele implements Comparable<tele>{
+	int no, dist = 1000000001;
+	tele(int no) {
+		this.no = no;
+	}
+	public int compareTo(tele o) {
+		return this.dist - o.dist;
+	}
+}
+
+class line{
+	int to,dist;
+	line(int to, int dist){
+		this.to = to;
+		this.dist = dist;
+	}
+}
